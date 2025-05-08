ae3980399fcf04db73ab911bc6c08d95fae96eed
Fariz Rahman Nst <farizrahmannst14@gmail.com>
2025-05-08 20:57:47 +0700

    Membuat tampilan list

--- app/src/main/java/com/fariznst0075/tandatonton/ui/screen/MainScreen.kt
+++ app/src/main/java/com/fariznst0075/tandatonton/ui/screen/MainScreen.kt
@@ -2,8 +2,15 @@ package com.fariznst0075.tandatonton.ui.screen
 
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,8 +19,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.fariznst0075.tandatonton.R
+import com.fariznst0075.tandatonton.model.Film
 import com.fariznst0075.tandatonton.ui.theme.TandaTontonTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -38,10 +50,38 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+
+    LazyColumn(
+        modifier = modifier.fillMaxSize()
+    ) {
+        items(data){
+            ListItem(film = it)
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun ListItem(film: Film) {
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        Text(
+            text = film.judul,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = film.tanggal,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(text = film.genre)
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/fariznst0075/tandatonton/ui/screen/MainViewModel.kt
+++ app/src/main/java/com/fariznst0075/tandatonton/ui/screen/MainViewModel.kt
@@ -8,37 +8,37 @@ class MainViewModel : ViewModel() {
     val data = listOf(
         Film(
             1,
-            "Kuliah Mobpro 17 Feb",
+            "Vinland Saga S1",
             "2025-02-17 12:34:56",
             "action"
         ),
         Film(
             2,
-            "Kuliah Mobpro 19 Feb",
+            "Prison School",
             "2025-02-19 12:34:56",
             "comedy"
         ),
         Film(
             3,
-            "Kuliah Mobpro 24 Feb",
+            "Prison School",
             "2025-02-23 12:34:56",
             "action"
         ),
         Film(
             4,
-            "Kuliah Mobpro 26 Feb",
+            "Prison School",
             "2025-02-26 12:34:56",
             "action"
         ),
         Film(
             5,
-            "Kuliah Mobpro 03 Mar",
+            "Prison School",
             "2025-03-03 12:34:56",
             "action"
         ),
         Film(
             6,
-            "Kuliah Mobpro 05 Mar",
+            "Vinland Saga S1",
             "2025-03-05 12:34:56",
             "action"
         )
