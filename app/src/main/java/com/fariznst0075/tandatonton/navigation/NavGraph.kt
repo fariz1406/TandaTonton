b3988b95831047d1cefb93995fcad3a53dfbfa2e
Fariz Rahman Nst <farizrahmannst14@gmail.com>
2025-05-08 21:56:14 +0700

    Membuat tampilan form catatan

--- app/src/main/java/com/fariznst0075/tandatonton/navigation/NavGraph.kt
+++ app/src/main/java/com/fariznst0075/tandatonton/navigation/NavGraph.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.fariznst0075.tandatonton.ui.screen.DetailScreen
 import com.fariznst0075.tandatonton.ui.screen.MainScreen
 
 @Composable
@@ -15,6 +16,9 @@ fun SetupNavGraph(navController: NavHostController = rememberNavController()) {
     ) {
         composable(route = Screen.Home.route) {
             MainScreen()
+        }
+        composable(route = Screen.FormBaru.route) {
+            DetailScreen()
         }
     }
 }
--- app/src/main/java/com/fariznst0075/tandatonton/navigation/Screen.kt
+++ app/src/main/java/com/fariznst0075/tandatonton/navigation/Screen.kt
@@ -2,4 +2,5 @@ package com.fariznst0075.tandatonton.navigation
 
 sealed class Screen(val route: String) {
     data object Home: Screen("mainScreen")
+    data object FormBaru: Screen("detailScreen")
 }
