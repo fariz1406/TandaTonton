561c1f7bbf5313c56792b77910afadec350705a8
Fariz Rahman Nst <farizrahmannst14@gmail.com>
2025-05-08 22:16:37 +0700

    Membuat navigasi ke form ubah data

--- app/src/main/java/com/fariznst0075/tandatonton/navigation/Screen.kt
+++ app/src/main/java/com/fariznst0075/tandatonton/navigation/Screen.kt
@@ -1,6 +1,11 @@
 package com.fariznst0075.tandatonton.navigation
 
+import com.fariznst0075.tandatonton.ui.screen.KEY_ID_CATATAN
+
 sealed class Screen(val route: String) {
     data object Home: Screen("mainScreen")
     data object FormBaru: Screen("detailScreen")
+    data object FormUbah: Screen("detailscreen/{$KEY_ID_CATATAN}"){
+        fun withId (id: Long) = "detailscreen/$id"
+    }
 }
